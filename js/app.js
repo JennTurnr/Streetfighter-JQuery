b4f46e62d1e6c0f6ba272230fbc0e03502df64c8
Jennifer Turner <jenniferturner202@gmail.com>
2014-06-01 10:14:38 -0400

    Added Hadouken sound and additional .ryu animation functionality.

--- js/app.js
+++ js/app.js
@@ -8,15 +8,28 @@ $('.ryu').mouseenter(function() {
 	$('.ryu-still').show();
 })
 .mousedown(function() {
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // animate hadouken to the right of the screen
+    $('.hadouken').finish().show()
+    .animate(
+    	{'left': '300px'},
+    	500,
+    	function() {
+    		$(this).hide();
+    		$(this).css('left', '-180px');
+    	}
+    	);
 })
 
-.mouseup(function(){
+.mouseup(function() {
 	$('.ryu-throwing').hide();
 	$('.ryu-ready').show();
 });
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
