3cda0d5d6512966696b2b3a7726ad256b8c530da
Jennifer Turner <jenniferturner202@gmail.com>
2014-06-03 18:07:57 -0400

    Working on keydown()

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ $('.ryu').mouseenter(function() {
 	$('.ryu-still').hide();
 	$('.ryu-ready').show();
 })
+
 .mouseleave(function() {
 	$('.ryu-ready').hide();
 	$('.ryu-still').show();
 })
+
 .mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
@@ -26,10 +28,33 @@ $('.ryu').mouseenter(function() {
 	$('.ryu-throwing').hide();
 	$('.ryu-ready').show();
 });
+
+$(document).keydown(function(event){
+    if (event.which == 88) {
+        //playSwagga();
+        alert("You did it!");
+        $('.ryu-ready').hide();
+        $('.ryu-cool').show();     
+}
+})
+.keyup(function(event){
+    if (event.which == 88) {
+        alert("You did it again!");
+       $('ryu-cool').hide();
+        $('.ryu-ready').show();
+    }
 });
+
 
 function playHadouken () {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 }
+
+function playSwagga(){
+    $('#swagga-like-us')[0].volume = 0.5;
+    $('#swagga-like-us')[0].load();
+    $('#swagga-like-us')[0].play();
+}
+});
