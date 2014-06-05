67a60f78d23f2df631facc0f574cf3121c3b9b89
Jennifer Turner <jenniferturner202@gmail.com>
2014-06-05 10:03:45 -0400

    Added .Ryu cool pose functionality
    Working on music playing during pose and re-implementing .ryu-throwing
    functionality.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 $('.ryu').mouseenter(function() {
 	$('.ryu-still').hide();
+    $('.ryu-cool').hide();
 	$('.ryu-ready').show();
 })
 
 .mouseleave(function() {
 	$('.ryu-ready').hide();
+    $('.ryu-cool').hide();
 	$('.ryu-still').show();
 })
 
 .mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show()
     .animate(
@@ -25,23 +28,27 @@ $('.ryu').mouseenter(function() {
 })
 
 .mouseup(function() {
-	$('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
 	$('.ryu-ready').show();
 });
+
 
 $(document).keydown(function(event){
     if (event.which == 88) {
         //playSwagga();
-        console.log("You did it!");
+        $('.ryu-throwing').hide();
+        $('.ryu-still').hide();
         $('.ryu-ready').hide();
-        $('.ryu-cool').show();     
+        $('.ryu-cool').show(); 
 }
-})
-.keyup(function(event){
+
+$(document).keyup(function(event){
     if (event.which == 88) {
-        console.log("You did it again!");
-       $('ryu-cool').hide();
-        $('.ryu-ready').show();
+       //console.log("You did it again!");
+      $('.ryu-cool').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-still').show();
     }
 });
 
@@ -52,9 +59,10 @@ function playHadouken () {
 	$('#hadouken-sound')[0].play();
 }
 
-function playSwagga(){
-    $('#swagga-like-us')[0].volume = 0.5;
-    $('#swagga-like-us')[0].load();
-    $('#swagga-like-us')[0].play();
-}
+function playSwagger () {
+   $('#swagger-like-us-sound')[0].volume = 0.5;
+   $('#swagger-like-us-sound')[0].load();
+   $('#swagger-like-us-sound')[0].play();
+  } 
+});
 });
